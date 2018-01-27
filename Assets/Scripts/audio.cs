e92694175941a916810eb4a7cbb2003fafc1e84e
Luísa <luisamey@hotmail.com>
2018-01-27 17:08:58 -0300

    Menu Scene

---
+++ Assets/Scripts/audio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audio : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad (transform.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
+
+
+
+
